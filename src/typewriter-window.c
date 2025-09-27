b426284c58ce7f6e651f760e70ecf15c7cd97165
Fenglu Niu <niufenglu@gmail.com>
2025-09-27 23:24:19 +0800

    feat:识别Q群赛文格式并处理

--- src/typewriter-window.c
+++ src/typewriter-window.c
@@ -447,23 +447,45 @@ static void load_clipboard_text(GdkClipboard *clipboard, GAsyncResult *result,
     return;
   }
 
-  if (text != NULL) {
-    // 成功获取到文本，可以在这里进行处理
-    GtkTextBuffer *buffer =
-        gtk_text_view_get_buffer(GTK_TEXT_VIEW(win->control));
-    gtk_text_buffer_set_text(buffer, text, -1);
-    gtk_label_set_text(GTK_LABEL(win->info), "来自剪贴板");
-    win->article_name = "来自剪贴板";
-    // 计算文本长度
-
-    int text_length = g_utf8_strlen(text, -1);
-    // 更新总字数标签
-    gtk_label_set_text(GTK_LABEL(win->words),
-                       g_strdup_printf("共%d字", text_length));
-
-    // 不要忘记释放文本资源
-    g_free(text);
+  if (text == NULL) {
+    return;
   }
+  // 成功获取到文本，可以在这里进行处理
+  gtk_label_set_text(GTK_LABEL(win->info), "来自剪贴板");
+  win->article_name = "来自剪贴板";
+  // 首先检测是否赛文格式
+  GError *regex_error = NULL;
+  GRegex *regex = g_regex_new("(.*?)\\n(.*?)\\n-----(第\\d+段)-共\\d+字.*?",
+                              G_REGEX_DEFAULT, 0, &regex_error);
+  if (regex_error != NULL) {
+    g_printerr("Error compiling regex: %s\n", regex_error->message);
+    g_error_free(regex_error);
+  }
+
+  if (regex_error == NULL) {
+    GMatchInfo *match_info = NULL;
+    g_regex_match(regex, text, 0, &match_info);
+    if (g_match_info_matches(match_info)) {
+      win->article_name = g_match_info_fetch(match_info, 3);
+      gtk_label_set_label(GTK_LABEL(win->info), g_strdup_printf("%s-%s", g_match_info_fetch(match_info, 1), win->article_name));
+      text = g_match_info_fetch(match_info, 2);
+      g_free(match_info);
+      g_free(regex);
+    }
+  }
+
+  GtkTextBuffer *buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(win->control));
+  gtk_text_buffer_set_text(buffer, text, -1);
+
+  // 计算文本长度
+
+  int text_length = g_utf8_strlen(text, -1);
+  // 更新总字数标签
+  gtk_label_set_text(GTK_LABEL(win->words),
+                     g_strdup_printf("共%d字", text_length));
+
+  // 不要忘记释放文本资源
+  g_free(text);
 }
 
 void load_file(TypewriterWindow *win) { g_assert(TYPEWRITER_IS_WINDOW(win)); }
